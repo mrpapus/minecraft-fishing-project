d8f24669825b5c16f04f51e44e5fc4d5c19968b0
mrpapus <srednalf05@icloud.com>
2023-02-06 20:30:01 -0700

    Update main.js

--- main.js
+++ main.js
@@ -56,7 +56,7 @@ function fishBtnClicked() {
       catchFish(numPuffer++,"Pufferfish",numPufferSpan)
     }
   } else if (character === "villager"){
-
+     //villager 25% for each
     let randNum = Math.random();
     if (randNum < 0.25) {
       catchFish(numCod++,"Raw-cod",numCodSpan)
@@ -69,82 +69,3 @@ function fishBtnClicked() {
     }
  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// // Event handler for fishBtn
-// function fishBtnClicked() {
-//   // Check Selected Character
-//   let character = charSelect.value;
-
-//   // Catch fish based on character
-//   if (character === 'steve') {
-//     // STEVE: Cod 70%, Salmon 20%, Tropical 5%, Puffer 5%
-//     let randNum = Math.random();
-//     if (randNum < 0.7) {
-//       numCod++;
-//       numCodSpan.innerHTML = numCod;
-//       resultImg.src = 'img/Raw-Cod.png';
-//     } else if (randNum < 0.9) {
-//       numSalmon++;
-//       numSalmonSpan.innerHTML = numSalmon;
-//       resultImg.src = 'img/Raw-Salmon.png';
-//     } else if (randNum < 0.95) {
-//       numTropical++;
-//       numTropicalSpan.innerHTML = numTropical;
-//       resultImg.src = 'img/Tropical-Fish.png';
-//     } else {
-//       numPuffer++;
-//       numPufferSpan.innerHTML = numPuffer;
-//       resultImg.src = 'img/Pufferfish.png';
-//     }
-//   } else if (character === 'alex') {
-//     // ALEX: Cod 10%, Salmon 10%, Tropical 30%, Puffer 50%
-//     let randNum = Math.random();
-//     if (randNum < 0.1) {
-//       numCod++;
-//       numCodSpan.innerHTML = numCod;
-//       resultImg.src = 'img/Raw-Cod.png';
-//     } else if (randNum < 0.2) {
-//       numSalmon++;
-//       numSalmonSpan.innerHTML = numSalmon;
-//       resultImg.src = 'img/Raw-Salmon.png';
-//     } else if (randNum < 0.5) {
-//       numTropical++;
-//       numTropicalSpan.innerHTML = numTropical;
-//       resultImg.src = 'img/Tropical-Fish.png';
-//     } else {
-//       numPuffer++;
-//       numPufferSpan.innerHTML = numPuffer;
-//       resultImg.src = 'img/Pufferfish.png';
-//     }
-//   }
-// }
